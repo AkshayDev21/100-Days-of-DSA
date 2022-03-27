9268f12293dd45b4dc6aa6c8fc9dc4d9d9f3a1c3
Akshay Nille <nilleakshay@gmail.com>
2022-03-27 19:30:00 +0530

    Day 2 of 30 days of DSA

---
+++ Day2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 'a';
+    cout<< a << endl;
+
+    int b = 2/5;
+    cout<< b << endl;
+
+    int c = 5;
+    int d = 6;
+
+    bool first = (c==d);
+    cout<< first << endl;
+
+    bool second = (c<d);
+    cout<< second << endl;
+
+    int e = 0;
+    cout<< !e <<endl;
+}
