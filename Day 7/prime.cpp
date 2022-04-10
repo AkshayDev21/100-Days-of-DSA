732aac5800838d8c79658eab2151029207581178
Akshay Nille <nilleakshay@gmail.com>
2022-04-10 09:26:51 +0530

    Prime

---
+++ Day 7/prime.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// 1 -> Prime 
+// 0 -> Not Prime
+bool isPrime(int n){
+    for(int i =2; i<n; i++){
+        if(n&i==0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+
+    if (isPrime(n)){
+        cout<<"is a prime number"<<endl;
+    }
+    cout<<"is not a prime number"<<endl;
+
+}
