b42d9b83909330655ace4efefdb335fcec3e5daa
Akshay Nille <nilleakshay@gmail.com>
2022-04-23 22:45:31 +0530

    OOP Problems

--- OOP/intro1.cpp
+++ OOP/intro1.cpp
@@ -24,16 +24,13 @@ class Hero {
     void setlevel(char  ch){
         level = ch;
     }
+    
+    Hero(){
+        cout<<"Destructor Called"<<endl;
+    }
 };
 
 int main() {
-    //create object
-    Hero akshay ;
-    cout<<"Akshay health is "<<akshay.gethealth()<<endl;
-    akshay.setHealth(90);
-    akshay.level = 'A';
 
-    cout<<"Health is : "<< akshay.gethealth() <<endl;
-    cout<<"Level is : "<< akshay.level << endl;
-    return 0;
+    Hero akshay;
 }
