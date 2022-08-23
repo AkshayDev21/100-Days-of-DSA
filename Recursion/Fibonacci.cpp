2ebc9aa0a9ccbf1b8433dc8cc27dafec5de38076
Akshay Nille <nilleakshay@gmail.com>
2022-08-23 11:59:43 +0530

    Fibonacci Series

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n){
+    //Base case 
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+
+    //Recursive Call
+    int call = fibo(n-1) + fibo(n-2);
+    return call;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int ans = fibo(n);
+    cout<<ans<<endl;
+    return 0;
+}
