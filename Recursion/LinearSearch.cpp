65efb36ad616958c22fc3ec0f6a044069337a276
Akshay Nille <nilleakshay@gmail.com>
2022-08-26 12:11:03 +0530

    Linear Search

---
+++ Recursion/LinearSearch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+bool linearSearch(int arr[], int size, int k){
+    if(size==0){
+        return false;
+    }
+    if(arr[0]==k){
+        return true;
+    }
+    else{
+        bool remainingPart = linearSearch(arr+1, size-1, k);
+        return remainingPart;
+    }
+}
+
+int main(){
+    cout<<"Enter the size of array :"<<endl;
+    int size;
+    cin>>size;
+
+    cout<<"Enter the array :"<<endl;
+    int arr[100];
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Enter the element to find it in array "<<endl;
+    int key;
+    cin>>key;
+
+    int ans = linearSearch(arr, size, key);
+
+    if(ans){
+        cout<<"Element Found "<<endl;
+    }
+    else{
+        cout<<"No element found"<<endl;
+    }
+    
+
+}
