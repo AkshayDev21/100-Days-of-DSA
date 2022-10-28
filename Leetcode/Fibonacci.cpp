5b05b24a78d6541aa1f762577c8e11fd21b9f00f
Akshay Nille <72347834+AkshayDev21@users.noreply.github.com>
2022-10-28 22:59:43 +0530

    Updating

--- Leetcode/Fibonacci.cpp
+++ Leetcode/Fibonacci.cpp
@@ -4,9 +4,11 @@ public:
         if(n < 2){
             return n;
         }
+
         vector vals = {0, 1};
         int i = 2;
         int sum = 1;
+
         while(i <= n){
             sum = vals[i-1] + vals[i-2];
             vals.push_back(sum);
