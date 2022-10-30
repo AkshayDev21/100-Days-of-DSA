c0c0b66afbe557921169d81ef0bc279819d1918e
Akshay Nille <72347834+AkshayDev21@users.noreply.github.com>
2022-10-30 18:42:15 +0530

    Updating

--- CodingNinja/CheckPalindrome.cpp
+++ CodingNinja/CheckPalindrome.cpp
@@ -4,14 +4,17 @@ using namespace std;
 bool checkPalindrome(char a[], int n){
     int s =0;
     int e=n-1;
+
     while(s<=e){
         if(a[s] != a[e]){
             return 0;
         }
+
         else{
             s++;
             e--;
         }
+
     }
     return 1;
 }
