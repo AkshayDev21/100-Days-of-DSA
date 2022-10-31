89dcb2d8b43284ff421bf7ce1ffa3aff9cfeb050
Akshay Nille <72347834+AkshayDev21@users.noreply.github.com>
2022-10-31 19:14:24 +0530

    Updated

--- Array/BinarySearch.cpp
+++ Array/BinarySearch.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int binarysearch(int arr[], int size, int key){
     int start = 0;
     int end = size-1;
-
     int mid = start + (end-start)/2;
-
     while(start<=end){
         if(arr[mid]==key){
             return mid;
