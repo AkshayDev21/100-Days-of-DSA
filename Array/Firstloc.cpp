cbb170fb2ce2c7ed63085eb6cd6372da233848c7
Akshay Nille <72347834+AkshayDev21@users.noreply.github.com>
2022-11-03 21:40:49 +0530

    Update Firstloc.cpp

--- Array/Firstloc.cpp
+++ Array/Firstloc.cpp
@@ -4,12 +4,12 @@ int firstOcc(int arr[], int n, int key){
     int s=0 , e = n-1;
     int mid = s + (e-s)/2;
     int ans = -1;
-
     while(s<=e){
         if(arr[mid] == key){
             ans = mid;
             e = mid-1;
         }
+        
         else if(key > arr[mid]){
             s = mid+1;
         }
@@ -19,6 +19,7 @@ int firstOcc(int arr[], int n, int key){
         mid = s + (e-s)/2;
     }
     return ans;
+    
 }
 
 int lastOcc(int arr[], int n, int key){
